4ab46210098159d802c458fc1f32677f5f1a2290
Jesper Söderlund <jesper.soderlund@gmail.com>
2015-10-06 04:49:56 +0200

    New version dev opened

--- src/main/java/com/jsoft/iot/mqttloadapp/ApplicationConfig.java
+++ src/main/java/com/jsoft/iot/mqttloadapp/ApplicationConfig.java
@@ -24,7 +24,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(com.jsoft.iot.mqttloadapp.rest.ConfigResource.class);
         resources.add(com.jsoft.iot.mqttloadapp.rest.ConfigsResource.class);
         resources.add(com.jsoft.iot.mqttloadapp.rest.MetricsSeriesCollectionResource.class);
         resources.add(com.jsoft.iot.mqttloadapp.rest.MetricsSeriesResource.class);
